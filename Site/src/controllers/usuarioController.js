da5c05d952e4b4f3ce0ab1cba6035d8c4c796f08
samuel <samuel.santos@sptech.school>
2023-11-26 00:02:40 -0300

    ajustes

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -22,7 +22,7 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                        aquarioModel.autenticar(resultadoAutenticar[0].empresaId)
                             .then((resultadoAquarios) => {
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
