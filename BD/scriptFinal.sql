85626790f3a039ebd911207310c6c0ac764218f7
samuel <samuel.santos@sptech.school>
2023-11-27 23:29:13 -0300

    ajustes

--- BD/scriptFinal.sql
+++ BD/scriptFinal.sql
@@ -4,46 +4,32 @@ create database if not exists projetoIndividual;
 
 use projetoIndividual;
 
-create table Relatorio (
-idRelatorio int primary key auto_increment,
-CNH char(3),
-dirige char(3),
-possuiMoto char(3)
-);
-
-create table Endereco (
-idEndereco int primary key auto_increment,
-Cep char(8),
-Rua varchar(45),
-Bairro varchar (45),
-Cidade varchar(45)
-);
-
 create table Quiz (
 idQuiz int primary key auto_increment,
-Resposta1 varchar(45),
-Resposta2 varchar(45),
-Resposta3 varchar(45),
-Resposta4 varchar(45),
-Resposta5 varchar(45)
+Acertos int,
+Erros int
 );
 
 create table Usuario (
 idUsuario int auto_increment,
-fkEndereco int,
-fkRelatorio int,
 fkQuiz int,
 Nome varchar(45),
 Email varchar(45),
 Senha varchar(45),
+CNH char(3),
+Dirige char(3),
+possuiMoto char(3),
 primary key (idUsuario),
-foreign key (fkEndereco) references Endereco(idEndereco),
-foreign key (fkRelatorio) references Relatorio(idRelatorio),
 foreign key (fkQuiz) references Quiz(idQuiz) 
 );
 
-select * from Usuario;
-
-select * from Endereco;
-
-select * from Relatorio;
+create table Endereco (
+idEndereco int auto_increment,
+fkUsuario int,
+Cep char(8),
+Rua varchar(45),
+Bairro varchar (45),
+Cidade varchar(45),
+primary key (idEndereco),
+foreign key (fkUsuario) references Usuario(idUsuario)
+);
